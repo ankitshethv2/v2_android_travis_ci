abda5ca1187a103702c9a8e0f6e348700e097973
Ankit Sheth <ankitsheth111@gmail.com>
2014-12-24 13:23:45 +0530

    first commit

---
+++ gen/com/example/v2_android_travis_ci/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.v2_android_travis_ci;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/v2_android_travis_ci/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.v2_android_travis_ci;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
